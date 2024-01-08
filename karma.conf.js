06f112e0612eed443029c4bbf610a6862eb0cf2e
Poonam Sharma <poonam.socialbox@gmail.com>
2024-01-07 19:21:01 -0600

    feat: created ngrx state counter

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-skeleton'),
+      dir: require('path').join(__dirname, './coverage/learn-ngrx'),
       subdir: '.',
       reporters: [
         { type: 'html' },
